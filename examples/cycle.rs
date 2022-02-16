3d995729ba00d58c5fc74204cf06df5950ac2a42
Krzysztof Biegański <krzysztof@biegan.ski>
2022-02-16 20:45:56 +0100

    Update to Bevy 0.6

--- examples/cycle.rs
+++ examples/cycle.rs
@@ -4,7 +4,7 @@ use bevy_flycam::PlayerPlugin;
 
 
 fn main() {
-    App::build()
+    App::new()
         .insert_resource(Msaa { samples: 4 })
         .insert_resource(AtmosphereMat::default())// Default AtmosphereMat, we can edit it to simulate another planet
         .add_plugins(DefaultPlugins)
@@ -16,27 +16,27 @@ fn main() {
 }
 
 // Marker for updating the position of the light, not needed unless we have multiple lights
+#[derive(Component)]
 struct Sun;
 
 // We can edit the SkyMaterial resource and it will be updated automatically, as long as ZephyrPlugin.dynamic is true
-fn daylight_cycle(mut sky_mat: ResMut<AtmosphereMat>, mut query: Query<&mut Transform, With<Sun>>, time: Res<Time>) {
+fn daylight_cycle(mut sky_mat: ResMut<AtmosphereMat>, mut query: Query<(&mut Transform, &mut DirectionalLight), With<Sun>>, time: Res<Time>) {
     let mut pos = sky_mat.sun_position;
     let t = time.time_since_startup().as_millis() as f32 / 2000.0;
     pos.y = t.sin();
     pos.z = t.cos();
     sky_mat.sun_position = pos;
 
-    // Since Bevy doesn't have directional lights, so we are just moving a point light around the cube.
-    if let Some(mut light_trans) = query.iter_mut().next() {
-        light_trans.translation = pos * 5.0;
+    if let Some((mut light_trans, mut directional)) = query.single_mut().into() {
+        light_trans.rotation = Quat::from_rotation_x(-pos.y.atan2(pos.z));
+        directional.illuminance = t.sin().max(0.0).powf(2.0) * 100000.0;
     }
 }
 
 // Simple environment
 fn setup_environment(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
     // Our Sun
-    commands.spawn_bundle(LightBundle {
-        transform: Transform::from_translation(Vec3::new(2.0, 1.0, 2.5)),
+    commands.spawn_bundle(DirectionalLightBundle {
         ..Default::default()
     })
         .insert(Sun); // Marks the light as Sun
--- src/shaders/sky.vert
+++ src/shaders/sky.vert
@@ -6,14 +6,22 @@ layout(location = 2) in vec2 Vertex_Uv;
 
 layout(location = 0) out vec3 v_Pos;
 
-
 layout(set = 0, binding = 0) uniform CameraViewProj {
     mat4 ViewProj;
-};
-layout(set = 1, binding = 0) uniform Transform {
-    mat4 Model;
+    mat4 InverseView;
+    mat4 Projection;
+    vec3 WorldPosition;
+    float near;
+    float far;
+    float width;
+    float height;
 };
 
+layout(set = 2, binding = 0) uniform Mesh {
+    mat4 Model;
+    mat4 InverseTransposeModel;
+    uint flags;
+};
 
 void main() {
     v_Pos = Vertex_Position;

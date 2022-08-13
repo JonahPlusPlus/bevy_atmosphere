ff042f87425067e1e45c5231c21dc07cb18d9017
Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>
2022-08-13 12:39:58 -0400

    Add "basic" example

---
+++ examples/basic.rs
@@ -0,0 +1,16 @@
+use bevy::prelude::*;
+use bevy_atmosphere::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(AtmospherePlugin::default())
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands
+        .spawn_bundle(Camera3dBundle::default())
+        .insert(AtmosphereCamera(0));
+}

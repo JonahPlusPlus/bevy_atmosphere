8bc340e0b4d25bf3a0fdec7167d4c3bf306e18fb
Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>
2022-08-13 14:12:21 -0400

    Updated example

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,25 @@
-//! A procedural sky plugin for bevy
+//! a procedural sky plugin for bevy
+//! 
 //!
-//! ## Example
+//! ## "basic" Example
 //! ```no_run
 //! use bevy::prelude::*;
-//! use bevy_atmosphere::*;
-//!
+//! use bevy_atmosphere::prelude::*;
+//! 
 //! fn main() {
 //!     App::new()
-//!         .insert_resource(bevy_atmosphere::AtmosphereMat::default()) // Default Earth sky
 //!         .add_plugins(DefaultPlugins)
-//!         .add_plugin(bevy_atmosphere::AtmospherePlugin {
-//!             dynamic: false,  // Set to false since we aren't changing the sky's appearance
-//!             sky_radius: 100.0
-//!         })
+//!         .add_plugin(AtmospherePlugin::default())
 //!         .add_startup_system(setup)
 //!         .run();
 //! }
-//!
+//! 
 //! fn setup(mut commands: Commands) {
-//!     commands.spawn_bundle(PerspectiveCameraBundle::default());
+//!     commands
+//!         .spawn_bundle(Camera3dBundle::default())
+//!         .insert(AtmosphereCamera(None));
 //! }
 //! ```
-
-#![allow(clippy::zero_prefixed_literal)] // ignore so we can make constant data look pretty
 
 pub mod pipeline;
 pub mod plugin;
